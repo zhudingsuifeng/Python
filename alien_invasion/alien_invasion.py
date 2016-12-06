722d83e26d7f6d9c745622f75c07578c60346209
注定随风 <1002557401@qq.com>
2016-12-06 16:05:45 +0800

    外星人入侵2.0,把一些设置选项单独放在一个文件的一个类中，这样管理设置就方便多了

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,12 +1,16 @@
 #coding=utf-8
 import sys
 import pygame
+from settings import Settings
 
 def run_game():
-	#初始化游戏并创建一个屏幕对象
+	#初始化pygame、设置和屏幕对象
 	pygame.init()
-	screen=pygame.display.set_mode((1200,800))
+	ai_settings=Settings()
+	screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
+	#设置背景颜色
+	bg_color=(230,230,230)
 
 	#开始游戏主循环
 	while True:
@@ -14,6 +18,8 @@ def run_game():
 		for event in pygame.event.get():
 			if event.type==pygame.QUIT:
 				sys.exit()
+		#每次循环时都重绘屏幕
+		screen.fill(ai_settings.bg_color)
 		#让最近绘制的屏幕可见
 		pygame.display.flip()
 
