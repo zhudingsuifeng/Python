38f6b7bb35f008604f15f5fadb98ab579265c835
注定随风 <1002557401@qq.com>
2016-12-06 17:05:37 +0800

    外星人入侵3.0，加入飞船

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
 	#初始化pygame、设置和屏幕对象
@@ -9,8 +10,8 @@ def run_game():
 	ai_settings=Settings()
 	screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
-	#设置背景颜色
-	bg_color=(230,230,230)
+	#创建一艘飞船
+	ship=Ship(screen)
 
 	#开始游戏主循环
 	while True:
@@ -20,6 +21,7 @@ def run_game():
 				sys.exit()
 		#每次循环时都重绘屏幕
 		screen.fill(ai_settings.bg_color)
+		ship.blitme()
 		#让最近绘制的屏幕可见
 		pygame.display.flip()
 
