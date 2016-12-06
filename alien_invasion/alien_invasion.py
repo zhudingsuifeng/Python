f757221a6d9a82f8e42846ec02b90beebe2d2ce3
注定随风 <1002557401@qq.com>
2016-12-06 19:23:34 +0800

    外星人入侵4.0，把屏幕刷新，监听事件分离出来

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+import game_functions as gf
 
 def run_game():
 	#初始化pygame、设置和屏幕对象
@@ -16,13 +17,8 @@ def run_game():
 	#开始游戏主循环
 	while True:
 		#监事键盘和鼠标事件
-		for event in pygame.event.get():
-			if event.type==pygame.QUIT:
-				sys.exit()
+		gf.check_events()
 		#每次循环时都重绘屏幕
-		screen.fill(ai_settings.bg_color)
-		ship.blitme()
-		#让最近绘制的屏幕可见
-		pygame.display.flip()
+		gf.update_screen(ai_settings,screen,ship)
 
 run_game()
