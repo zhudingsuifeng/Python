0b41f27a7abb4133e07dd5f74b7d5122f6caa3ca
注定随风 <1002557401@qq.com>
2016-12-09 20:41:35 +0800

    外星人入侵9.0，检测飞船外星人碰撞，外星人到达底部，子弹和外星人碰撞等基本完成

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -3,8 +3,10 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 from pygame.sprite import Group
+from game_stats import GameStats
 
 def run_game():
 	#初始化pygame、设置和屏幕对象
@@ -12,17 +14,25 @@ def run_game():
 	ai_settings=Settings()
 	screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
-	#创建一艘飞船
+	#创建一个用于存储游戏统计信息的实例
+	stats=GameStats(ai_settings)
+	#创建一艘飞船、一个子弹编组和一个外星人编组
 	ship=Ship(ai_settings,screen)
 	#创建一个用于存储子弹的编组
 	bullets=Group()
+	aliens=Group()
+	#创建外星人群
+	gf.create_fleet(ai_settings,screen,ship,aliens)
 	#开始游戏主循环
 	while True:
 		#监事键盘和鼠标事件
 		gf.check_events(ai_settings,screen,ship,bullets)
-		ship.update()
-		gf.update_bullets(bullets)
+		if stats.game_active:
+			ship.update()
+			gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
+			gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
+		
 		#每次循环时都重绘屏幕
-		gf.update_screen(ai_settings,screen,ship,bullets)
+		gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 
 run_game()
