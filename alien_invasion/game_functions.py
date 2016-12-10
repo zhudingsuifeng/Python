6b656407a0cf1e7aefba9e81932593a73361797b
注定随风 <1002557401@qq.com>
2016-12-10 21:30:22 +0800

    外星人入侵10.0，添加play按钮，当没有命之后，暂停，再次显示play按钮

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -21,7 +21,7 @@ def check_keyup_events(event,ship):
 		ship.moving_right=False
 	elif event.key==pygame.K_LEFT:
 		ship.moving_left=False
-def check_events(ai_settings,screen,ship,bullets):
+def check_events(ai_settings,screen,stats,play_button,ship,aliens,bullets):
 	"""响应按键和鼠标事件"""
 	for event in pygame.event.get():
 		if event.type==pygame.QUIT:
@@ -30,8 +30,27 @@ def check_events(ai_settings,screen,ship,bullets):
 			check_keydown_events(event,ai_settings,screen,ship,bullets)
 		elif event.type==pygame.KEYUP:
 			check_keyup_events(event,ship)
+		elif event.type==pygame.MOUSEBUTTONDOWN:
+			mouse_x,mouse_y=pygame.mouse.get_pos()
+			check_play_button(ai_settings,screen,stats,play_button,ship,aliens,bullets,mouse_x,mouse_y)
 
-def update_screen(ai_settings,screen,ship,aliens,bullets):
+def check_play_button(ai_settings,screen,stats,play_button,ship,aliens,bullets,mouse_x,mouse_y):
+	"""在玩家单击Play按钮时开始新游戏"""
+	button_clicked=play_button.rect.collidepoint(mouse_x,mouse_y)
+	if button_clicked and not stats.game_active:
+		#隐藏光标
+		pygame.mouse.set_visible(False)
+		#重置游戏统计信息
+		stats.reset_stats()
+		stats.game_active=True
+		#清空外星人列表和子弹列表
+		aliens.empty()
+		bullets.empty()
+		#创建一群新的外星人，并让飞船居中
+		create_fleet(ai_settings, screen, ship, aliens)
+		ship.center_ship()
+
+def update_screen(ai_settings,screen,stats,ship,aliens,bullets,play_button):
 	"""更新屏幕上的图像，并切换到新屏幕"""
 	#每次循环时都重绘屏幕
 	screen.fill(ai_settings.bg_color)
@@ -40,7 +59,9 @@ def update_screen(ai_settings,screen,ship,aliens,bullets):
 		bullet.draw_bullet()
 	ship.blitme()
 	aliens.draw(screen)
-
+	#如果游戏处于非活动状态，就绘制play按钮
+	if not stats.game_active:
+		play_button.draw_button()
 	#让最近绘制的屏幕可见
 	pygame.display.flip()
 def update_bullets(ai_settings,screen,ship,aliens,bullets):
@@ -130,6 +151,7 @@ def ship_hit(ai_settings,stats,screen,ship,aliens,bullets):
 		sleep(0.5)
 	else:
 		stats.game_active=False	
+		pygame.mouse.set_visible(True)
 
 def check_aliens_bottom(ai_settings,stats,screen,ship,aliens,bullets):
 	"""检查是否有外星人到达了屏幕底端"""
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -18,7 +18,7 @@ class Settings():
 		self.bullets_allowed=3
 		#外星人设置
 		self.alien_speed_factor=1
-		self.fleet_drop_speed=10
+		self.fleet_drop_speed=50
 		#fleet_direction为1表示向右移，为-1表示向左移
 		self.fleet_direction=1
 		
