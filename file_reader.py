e53f264888d25229d8bed7dfa4c84051bca6f052
注定随风 <1002557401@qq.com>
2017-09-26 21:16:17 +0800

    imit

--- file_reader.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-#coding=utf-8
-filename='pi.txt'
-with open(filename) as file_object:
-#    for line in file_object:   line并不是关键字，对文件对象使用for in默认时一行一行的 
-    for s in file_object:
-	print(s)
--- functiontest.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-#coding=utf-8
-import numpy
-arr=[[1,2],[3,4]]
-data=list(arr)
-print data
