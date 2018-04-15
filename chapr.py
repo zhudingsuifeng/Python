2b46d7389416e0b40ad6fc1dfc7e3ef92cad4a72
zhudingsuifeng <1002557401@qq.com>
2018-04-15 22:07:48 +0800

    get the most occurrences of the three characters in the input string and output

---
+++ chapr.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+#coding=utf-8
+"""
+Create on Mon Oct 30 21:46:32 2017
+@author:fly
+Using pca analyze stock data.
+"""
+import os
+import sys
+from collections import Counter
+import numpy as np
+import math
+
+def chapr(strch):
+    strs=Counter(strch)
+    result=sorted(strs.items(),key=lambda k:k[1],reverse=True)
+    ch=result[0][0]+result[1][0]+result[2][0]
+    for i in range(2,len(result)):
+    	if result[i][1]==result[i+1][1]:
+	    ch+=result[i+1][0]
+	else:
+	    return ch
+strs=raw_input()
+print(chapr(strs))
