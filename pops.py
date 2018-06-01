9546fd8b44461bd8b6a38b316e1d7b4d38ebac8e
zhudingsuifeng <1002557401@qq.com>
2018-06-01 09:55:29 +0800

    backup

---
+++ pops.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+#coding=utf-8
+"""
+Create on Fri May 18 16:08:56 2018
+@author:fly
+devops
+"""
+import os
+import sys
+import filecmp
+import shutil
+
+
+
+
+if __name__=='__main__':
+	print("success")
