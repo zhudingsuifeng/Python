a51126ec8d6481f682eb03a44cf618e54276ac6b
fly <1002557401@qq.com>
2018-09-07 21:27:41 +0800

    advanced question of recursive summation

--- code/sequence.py
+++ code/sequence.py
@@ -12,15 +12,15 @@ def pair(A,n,k):
     if n in A:
         i=A.index(n)  #number n in list last
         if i==n-1:  
-            return pair(A[:n-1],n-1,k-n+1)
+            return pair(A[:n-1],n-1,k-n+1)  #because of counting number of pairs ,we need return 0,1
         else:
             return pair(A[:i]+A[i+1:],n-1,k-i)
-    else:
+    else:             #number n is not in list 
         res=0
         for j in range(n):
             if A[j]==0:
                 A[j]=n
-                res+=pair(A,n,k)
+                res+=pair(A,n,k)  #count number of ordered pair if A[j]==0
                 A[j]=0
         return res
 
