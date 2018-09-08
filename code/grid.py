2242cdba5da6ddb20e2eebb1337edff7121eb6a7
fly <1002557401@qq.com>
2018-09-08 10:52:31 +0800

    node is not grid

--- code/grid.py
+++ code/grid.py
@@ -2,4 +2,17 @@
 #coding=utf-8
 #python3.6.5
 
+#from math import factorial   #factorial
+#x,y=map(int,input().split())
+#print(factorial(x+y)//factorial(x)//factorial(y))
+#This is a mathematical question
 
+def _path(x,y):
+    if x==0 or y==0:
+        return 1
+    else:
+        return _path(x-1,y)+_path(x,y-1)
+
+x,y=map(int,input().split())
+res=_path(x,y)
+print(res)
