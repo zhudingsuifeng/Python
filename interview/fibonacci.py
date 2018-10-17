8ef1268dca4fd5ff9ea4e73e9c7995b4f35bb2be
fly <1002557401@qq.com>
2018-10-17 10:18:38 +0800

    start from 0

--- interview/fibonacci.py
+++ interview/fibonacci.py
@@ -4,9 +4,12 @@
 class Solution:
     # @classmethod
     def Fibonacci(self,n):
-        l = [0]*n
-        l[0], l[1] = 1, 1
-        for i in range(2, n):
+        # starting from 0
+        if n < 2:
+            return n
+        l = [0]*(n+1)
+        l[1], l[2] = 1, 1
+        for i in range(3, n+1):
             l[i] = l[i-1] + l[i-2]
         return l[-1]
 
