f1e1d9f9ddb7da427cf2608745061f7a40126a8a
fly <1002557401@qq.com>
2019-04-15 19:48:43 +0800

    pythonic coding 2018

--- 2018/product.py
+++ 2018/product.py
@@ -2,36 +2,27 @@
 #coding = utf-8
 
 def product(l):
-    l = list(map(int, l.split()))
-    if len(l) < 3:
-        return 0
-    m, n, x, y, z = 0, 0, 0, 0, 0
-    for i in l:
-        if i == 0:
-            continue
-        elif i > m:
-            x = n
-            n = m
-            m = i
+    m, n, x, y, z = l[0], l[0], l[0], l[0], l[0]
+    for i in l[1:]:
+        if i > m:
+            m, n, x = i, m, n
         elif i > n:
-            x = n
-            n = i
+            n, x = i, n
         elif i > x:
             x = i
         elif i < z:
-            y = z
-            z = i
+            y, z = i, y
         elif i < y:
             y = i
     return max(m*n*x, m*y*z)
 
 def pro(l):
-    l = list(map(int, l.split()))
     if len(l) < 3:
         return 0
     l = sorted(l)
     return max(l[-1]*l[-2]*l[-3], l[-1]*l[0]*l[1])
 
 if __name__ == "__main__":
-    l = input()
+    _ = input()
+    l = list(map(int, input().split()))
     print(pro(l))
